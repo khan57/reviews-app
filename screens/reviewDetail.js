1b20d9251929e0ac3ea0987bcb919a4885d4aa88
khan57 <haseeb.ktk57@gmail.com>
2021-04-05 22:23:11 +0500

    project structure/BasicScreensAdded

---
+++ screens/reviewDetail.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function reviewDetail() {
+  return (
+    <View style={styles.container}>
+      <Text>ReviewDetail Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});
