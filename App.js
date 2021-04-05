93f9a64d0b4a4c5b3b762fd60b5079316f01197f
khan57 <haseeb.ktk57@gmail.com>
2021-04-05 23:00:49 +0500

    customFonts&GlobalStylesAdded

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./screens/Home";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
 
+const getFonts = () =>
+  Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Bold.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
 export default function App() {
-  return <Home />;
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  if (fontsLoaded) {
+    return <Home />;
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
 }
